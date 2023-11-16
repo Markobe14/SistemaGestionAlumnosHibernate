d7eadc69b744e2349d4ce71b1a71ddd8fbc40f4d
Marco Aguirre <alejandro.31198@gmail.com>
2023-11-15 18:23:17 -0600

    Creacion clases GenericDAO DomicilioDAO y ContactoDAO

--- src/main/java/mx/markobe/dao/ContactoDAO.java
+++ src/main/java/mx/markobe/dao/ContactoDAO.java
@@ -1,27 +1,81 @@
 package mx.markobe.dao;
 
 import java.util.List;
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
 import javax.persistence.Query;
 import mx.markobe.domain.Contacto;
 
-public class ContactoDAO {
+public class ContactoDAO extends GenericDAO{
     
-    private EntityManagerFactory emf;
-    private EntityManager em;
-    
-    public ContactoDAO(){
-        emf = Persistence.createEntityManagerFactory("HibernatePU");
-        em = emf.createEntityManager();
-    }
-    
-    public List<Contacto> listarContactos(){
-        String hql = "Select c FROM Contacto d";
-        Query query = em.createQuery(hql);
+    public List<Contacto> listarContacto() {
+
+        String consulta = "SELECT c FROM Contacto c";
+        em = getEntityManager();
+        Query query = em.createQuery(consulta);
+
         List<Contacto> contactos = query.getResultList();
-        
+
         return contactos;
     }
+
+    public void insertarContacto(Contacto contacto) {
+        try {
+            em = getEntityManager();
+            em.getTransaction().begin();
+            em.persist(contacto); //Persist es para insertar elementos
+            em.getTransaction().commit();
+            
+        } catch (Exception ex) {
+            ex.printStackTrace(System.out);
+            
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
+
+    }
+    
+    public void actualizarContacto(Contacto contacto) {
+        try {
+            em = getEntityManager();
+            em.getTransaction().begin();
+            em.merge(contacto); //Merge es para modificar
+            em.getTransaction().commit();
+            
+        } catch (Exception ex) {
+            ex.printStackTrace(System.out);
+            
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
+
+    }
+    
+    public void eliminarContacto(Contacto contacto) {
+        try {
+            em = getEntityManager();
+            em.getTransaction().begin();
+            em.remove(em.merge(contacto)); //Remove y merge, primero actualiza el estado de la BD y luego lo elimina
+            em.getTransaction().commit();
+            
+        } catch (Exception ex) {
+            ex.printStackTrace(System.out);
+            
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
+
+    }
+    
+    public Object buscarContactoPorId(Contacto contacto){
+        
+        em = getEntityManager();
+        return em.find(Contacto.class, contacto.getIdContacto());
+        
+    }
+    
 }
--- src/main/java/mx/markobe/dao/DomicilioDAO.java
+++ src/main/java/mx/markobe/dao/DomicilioDAO.java
@@ -1,27 +1,82 @@
 package mx.markobe.dao;
 
 import java.util.List;
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
 import javax.persistence.Query;
 import mx.markobe.domain.Domicilio;
 
-public class DomicilioDAO {
-    private EntityManagerFactory emf;
-    private EntityManager em;
-    
-    public DomicilioDAO(){
-        emf = Persistence.createEntityManagerFactory("HibernateJpaPU");
-        em = emf.createEntityManager();
-    }
-    
-    public List<Domicilio> listarDomicilios(){
-        String hql = "Select d FROM Domicilio d";
-        Query query = em.createQuery(hql);
+public class DomicilioDAO extends GenericDAO {
+
+    public List<Domicilio> listarDomicilios() {
+
+        String consulta = "SELECT d FROM Domicilio d";
+        em = getEntityManager();
+        Query query = em.createQuery(consulta);
+
         List<Domicilio> domicilios = query.getResultList();
-        
+
         return domicilios;
     }
+
+    public void insertarDomicilio(Domicilio domicilio) {
+        try {
+            em = getEntityManager();
+            em.getTransaction().begin();
+            em.persist(domicilio); //Persist es para insertar elementos
+            em.getTransaction().commit();
+            
+        } catch (Exception ex) {
+            ex.printStackTrace(System.out);
+            
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
+
+    }
+    
+    public void actualizarDomicilio(Domicilio domicilio) {
+        try {
+            em = getEntityManager();
+            em.getTransaction().begin();
+            em.merge(domicilio); //Merge es para modificar
+            em.getTransaction().commit();
+            
+        } catch (Exception ex) {
+            ex.printStackTrace(System.out);
+            
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
+
+    }
+    
+    public void eliminarDomicilio(Domicilio domicilio) {
+        try {
+            em = getEntityManager();
+            em.getTransaction().begin();
+            em.remove(em.merge(domicilio)); //Remove y merge, primero actualiza el estado de la BD y luego lo elimina
+            em.getTransaction().commit();
+            
+        } catch (Exception ex) {
+            ex.printStackTrace(System.out);
+            
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
+
+    }
+    
+    public Object buscarDomicilioPorId(Domicilio domicilio){
+        
+        em = getEntityManager();
+        return em.find(Domicilio.class, domicilio.getIdDomicilio());
+        
+    }
+
     
 }
